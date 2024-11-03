d1f4b944b2b7560562317a42fca9464f53a54b20
Basti <bastigepp@googlemail.com>
2024-11-03 19:28:50 +0100

    several changes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let DB_URL = "https://pokeapi.co/api/v2/";
 let allPokemon = [];
-let allPokemonNames = [];
+let currentIndex=[];
+let species = [];
 
 let offset = 0;
-let limit = 10;
+let limit = 20;
 
 function init(){
     fetchData();
@@ -20,25 +21,46 @@ async function fetchData(path = ""){
     return await response.json();
   }
 
-// async function getNames() {
-//     let namesResponse = await fetchData(`/pokemon/?offset=${offset}&limit=${limit}`);
-//     let names = await namesResponse.results;
-
-//     for (let i = 0; i < names.length; i++) {
-//         const element = names[i];
-//         let pokeData = await fetchPokeData(element.url)
-//         pokeData.name = capitalizeFirstLetter(pokeData.name);
-//         allPokemon.push(pokeData);
-//         document.getElementById('content').innerHTML += card(pokeData, i);
-//     }
+//   async function fetchEvolution(currentIndex){
+//     let url = allPokemon[currentIndex].species.url;
+//     console.log(url);
+//     let evolution = await fetch(url);
+//     return await evolution.json(); 
 //   }
+
+
+  async function fetchEvolution(currentIndex) {
+    let url = allPokemon[currentIndex].species.url;
+    
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+      fetchEvolutionChain(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+   
+  }
+
+  async function fetchEvolutionChain(json){
+    let url = json.evolution_chain.url;
+    console.log(url);
+    
+  }
+
+
+
 
 async function getNames(filter = "") {
     let namesResponse = await fetchData(`/pokemon/?offset=${offset}&limit=${limit}`);
     let names = await namesResponse.results;
     allPokemon = [];
     document.getElementById('content').innerHTML = '';
-    allPokemonNames.push(names);
 
     for (let i = 0; i < names.length; i++) {
         const element = names[i];
@@ -52,13 +74,12 @@ async function getNames(filter = "") {
     }
 }
 
-// Diese Funktion wird aufgerufen, wenn sich das Suchfeld ändert
 function searchPokemon() {
-    let filter = document.getElementById('search').value;
-    if (filter.length >= 3) {
-        getNames(filter); // Namen mit Suchbegriff filtern
-    } else if (filter.length === 0){
-        getNames(); // Alle Namen anzeigen, wenn weniger als 3 Zeichen eingegeben wurden
+    let searchWord = document.getElementById('search').value;
+    if (searchWord.length >= 3) {
+        getNames(searchWord);
+    } else if (searchWord.length === 0){
+        getNames();
     }
 }
 
@@ -71,13 +92,22 @@ function card(pokemon, index){
 }
 
 function openOverlay(index){
+    currentIndex = index;
     let overlay = document.getElementById('overlay');
     if (overlay.classList.contains('d-none')) {
-        // renderStats(index);
-        bigCardImg(index);
-        showStatsTab(index);
+        // bigCardImg(index);
+        // showStatsTab(index);
+        renderPokemon();
     }
     overlay.classList.toggle('d-none');
+}
+
+function renderPokemon(){
+    bigCardImg(currentIndex);
+    showStatsTab(currentIndex);
+    showAbilitiesTab(currentIndex);
+    calculatePokemonHight(currentIndex);
+    fetchEvolution(currentIndex);
 }
 
 function renderStats(index){
@@ -93,12 +123,37 @@ function bigCardImg(index) {
 }
 
 function loadMorePokemon(){
-    limit = limit + 20;
+    limit = limit + 10;
     document.getElementById('content').innerHTML = '';
     init();
 }
 
 function showStatsTab(index){
     let pokemon = allPokemon[index];
-    document.getElementById('nav-home').innerHTML = statsTab(pokemon);
+    document.getElementById('nav-stats').innerHTML = statsTab(pokemon);
 }
+
+function showAbilitiesTab(index){
+    let pokemon = allPokemon[index];
+    document.getElementById('nav-profile').innerHTML = abilitiesTab(pokemon);
+}
+
+function nextPokemon(){
+    if (currentIndex < allPokemon.length - 1){
+        currentIndex++;
+        renderPokemon();
+    }
+}
+
+function prevPokemon(){
+    if (currentIndex > 0){
+        currentIndex--;
+        renderPokemon();
+    }
+}
+
+function calculatePokemonHight(pokemon){
+    let height = (pokemon.height / 10).toFixed(2);
+    return height;
+}
+
--- templates/templates.js
+++ templates/templates.js
@@ -1,61 +1,11 @@
 function getCardTemplate(pokemon, index){
-    return  `<div class="card m-4" style="width: 18rem; --bs-card-border-width:none;" onclick="openOverlay(${index})">
+    return  `<div class="card m-4" style="width: 18rem; --bs-card-border-width:none;" onclick="openOverlay(${index})" id="pokemon_${index}>
                 <h4 class="card-title text-center">${pokemon.name}</h4>
                 <div id="imgContainer" class="${pokemon.types[0].type.name}">           
                     <img src="${pokemon.sprites.other.dream_world.front_default}" class="card-img-top mx-auto pt-2" alt="...">
                 </div>
                 <div class="card-body">
-                    <table class="table">
-                    <tbody>
-                        <tr>
-                            <td>Größe</td>
-                            <td>${pokemon.height} cm</td>
-                        </tr>
-                        <tr>
-                            <td>Gewicht</td>
-                            <td>${pokemon.weight} kg</td>
-                        </tr>
-                        <tr>
-                            <td>Basiserfahrung</td>
-                            <td>${pokemon.base_experience}</td>
-                        </tr>
-                        <tr>
-                            <td>Fähigkeit</td>
-                            <td>${pokemon.abilities[0].ability.name}</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>`
-}
-
-function getStatsTemplate(pokemon){
-    return `<h4>${pokemon.name}</h4>
-            <div class="progress-container">
-                <span>Base-HP:</span>
-                <div class="progress mt-3" role="progressbar" aria-valuenow="${pokemon.stats[0].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                    <div class="progress-bar" style="width: ${pokemon.stats[0].base_stat}%;"></div>
-                </div>
-            </div>
-
-            <div class="progress-container">
-                <span>Attack:</span>
-                <div class="progress mt-3" role="progressbar" aria-valuenow="${pokemon.stats[1].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                    <div class="progress-bar" style="width: ${pokemon.stats[1].base_stat}%;"></div>
-                </div>
-            </div>
-
-            <div class="progress-container">
-                <span>Defense:</span>
-                <div class="progress mt-3" role="progressbar" aria-valuenow="${pokemon.stats[2].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                    <div class="progress-bar" style="width: ${pokemon.stats[2].base_stat}%;"></div>
-                </div>
-            </div>
-
-            <div class="progress-container">
-                <span>Special Attack:</span>
-                <div class="progress mt-3" role="progressbar" aria-valuenow="${pokemon.stats[3].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                    <div class="progress-bar" style="width: ${pokemon.stats[3].base_stat}%;"></div>
+                    
                 </div>
             </div>`
 }
@@ -64,29 +14,53 @@ function statsTab(pokemon){
         return `
            <div class="progress-container">
                 <span>Base-HP:</span>
-                <div class="progress mt-3" role="progressbar" aria-valuenow="${pokemon.stats[0].base_stat}" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress" role="progressbar" aria-valuenow="${pokemon.stats[0].base_stat}" aria-valuemin="0" aria-valuemax="100">
                     <div class="progress-bar" style="width: ${pokemon.stats[0].base_stat}%;"></div>
                 </div>
             </div>
 
             <div class="progress-container">
                 <span>Attack:</span>
-                <div class="progress mt-3" role="progressbar" aria-valuenow="${pokemon.stats[1].base_stat}" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress" role="progressbar" aria-valuenow="${pokemon.stats[1].base_stat}" aria-valuemin="0" aria-valuemax="100">
                     <div class="progress-bar" style="width: ${pokemon.stats[1].base_stat}%;"></div>
                 </div>
             </div>
 
             <div class="progress-container">
                 <span>Defense:</span>
-                <div class="progress mt-3" role="progressbar" aria-valuenow="${pokemon.stats[2].base_stat}" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress" role="progressbar" aria-valuenow="${pokemon.stats[2].base_stat}" aria-valuemin="0" aria-valuemax="100">
                     <div class="progress-bar" style="width: ${pokemon.stats[2].base_stat}%;"></div>
                 </div>
             </div>
 
             <div class="progress-container">
                 <span>Special Attack:</span>
-                <div class="progress mt-3" role="progressbar" aria-valuenow="${pokemon.stats[3].base_stat}" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress" role="progressbar" aria-valuenow="${pokemon.stats[3].base_stat}" aria-valuemin="0" aria-valuemax="100">
                     <div class="progress-bar" style="width: ${pokemon.stats[3].base_stat}%;"></div>
                 </div>
             </div> `
+}
+
+function abilitiesTab(pokemon){
+    return `
+            <table class="table">
+            <tbody>
+                    <tr>
+                    <td>Größe</td>
+                    <td>${calculatePokemonHight(pokemon)} m</td>
+                </tr>
+                <tr>
+                    <td>Gewicht</td>
+                    <td>${pokemon.weight} kg</td>
+                </tr>
+                <tr>
+                    <td>Basiserfahrung</td>
+                    <td>${pokemon.base_experience}</td>
+                </tr>
+                <tr>
+                    <td>Fähigkeit</td>
+                    <td>${pokemon.abilities[0].ability.name}</td>
+                </tr>
+                </tbody>
+            </table>`
 }
